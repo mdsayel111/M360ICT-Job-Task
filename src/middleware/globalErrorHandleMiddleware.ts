650a1f889390633f1fbd02252e74abdab890642c
sayel <mdsayel111@gmail.com>
2025-04-28 21:53:12 +0600

    add global error handler"

---
+++ src/middleware/globalErrorHandleMiddleware.ts
@@ -0,0 +1,44 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint  no-unused-vars: "error" */
+import { ErrorRequestHandler } from "express";
+import { TErrorObj } from "../types/TError";
+
+// global error handle middleware
+const globalErrorHandleMiddleware: ErrorRequestHandler = (
+  err,
+  req,
+  res,
+  next,
+) => {
+  // default errObj
+  const errObj: TErrorObj = {
+    statusCode: 500,
+    message: "Something went wrong !",
+    errorMessages: [
+      {
+        path: "",
+        message: err.message,
+      },
+    ],
+    stack: "",
+  };
+
+  // if server run in production delete stack from errObj, so stack doesn't send with response
+  if (process.env.NODE_ENV === "production") {
+    delete errObj.stack;
+  }
+
+  // if status comes for authentication, then set statusCode to errObj
+  if (err.status === 401) {
+    errObj.statusCode = 401;
+  }
+
+  console.log(err);
+
+  // send response if any error occur
+  res.status(errObj.statusCode).send({ success: false, ...errObj });
+};
+
+export default globalErrorHandleMiddleware;
